d1bc3636bca75ea7c4d571b2b008424c8268c75c
knshika <poojakumari17032001@gmail.com>
2022-03-08 21:51:52 +0530

    🎉  basic firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import firebase from "./firebase/index"
 
 function App() {
+	console.log(firebase)
 	return (
-		<div>welcome</div>
+		<>
+			<div>welcome</div>
+			<div>{console.log(firebase)}</div>
+		</>
+	
 	)
 }
 
