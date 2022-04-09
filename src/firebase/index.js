a7f9d91fcb2bd7f6de70d71a6961a3add4087a3b
knshika <poojakumari17032001@gmail.com>
2022-04-10 01:25:18 +0530

    idk

--- src/firebase/index.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
-
-export const app = initializeApp(firebaseConfig)
-export const db = getFirestore()
